12c175386dd356fae27599eec51cfeb671c19eb4
Navindineshgautam <94467366+Navindineshgautam@users.noreply.github.com>
2023-01-15 22:19:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -104,7 +104,7 @@ function setValue(selector, value, {parent = document} = {}){
 }
 
 function getIconUrl(iconCode){
-    return `icons/${ICON_MAP.get(iconCode)}.svg`
+    return `${ICON_MAP.get(iconCode)}.svg`
 }
 
 const currentIcon = document.querySelector("[data-current-icon]");
